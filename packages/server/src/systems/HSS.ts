dca0582e054c9d36461348c0c08e738f35880ea0
David Allais <davidallais2001@gmail.com>
2023-06-13 14:58:06 +0200

    Update HSS.ts

--- packages/server/src/systems/HSS.ts
+++ packages/server/src/systems/HSS.ts
@@ -1,6 +1,43 @@
-// import fs from 'fs';
-// import path from 'path';
+import fs from 'fs';
+import path from 'path';
 
+
+/**
+ *List all json files in a directory
+ * @param dir
+ * @returns jsonFiles
+ */
+export function  getJsonFiles(dir: string) {
+    const files = fs.readdirSync(dir);
+    const jsonFiles: string[] = [];
+
+    for (const file of files) {
+        if (file.endsWith(".json")) {
+            jsonFiles.push(file);
+        }
+    }
+
+    return jsonFiles;
+}
+
+/**
+ * Search json files in a directory
+ * @param dir
+ * @param query
+ * @returns jsonFiles
+ */
+export function searchJsonFiles(dir: string, query: string) {
+    const files = fs.readdirSync(dir);
+    const jsonFiles: string[] = [];
+
+    for (const file of files) {
+        if (file.endsWith(".json") && file.includes(query)) {
+            jsonFiles.push(file);
+        }
+    }
+
+    return jsonFiles;
+}
 
 // Le chemin par défault, c'est path.join(__dirname, "..", "json"),
 // Ou __dirname est le dossier courant, donc packages\server\src\systems
